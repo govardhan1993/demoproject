e58dda48b86dd4cf633fa8cdff72af5619202b9b
Govardhan <jadhav.govardhan93@gmail.com>
2022-12-24 15:33:51 +0530

    first commit array list

---
+++ src/Firstprogram.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<String>();
+		
+		list.add("Govardhan");
+		list.add("jadhav");
+		System.out.println(list);
+	}
+
+}
